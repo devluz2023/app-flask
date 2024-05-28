6f0a94507fa442b3c0afb64c231258c84c200fc5
fabio-luz-ey <108548715+fabio-luz-ey@users.noreply.github.com>
2024-05-28 00:42:04 -0300

    ajustes

--- flasky.py
+++ flasky.py
@@ -66,7 +66,8 @@ def profile(length, profile_dir):
     from werkzeug.contrib.profiler import ProfilerMiddleware
     app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
                                       profile_dir=profile_dir)
-    app.run()
+    # app.run()
+    app.run(debug=True)
 
 
 @app.cli.command()
